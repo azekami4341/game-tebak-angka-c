ff6c7a5f405c4601bba34443c95ef5c8bcb87ce2
azekami4341 <vannesskenza@gmail.com>
2026-04-06 23:43:40 +0700

    changing language to english

--- ProgramTolol.c
+++ ProgramTolol.c
@@ -5,19 +5,19 @@ int main(){
     int y = 69;
 
     while (x != y ){
-        printf ("Tebak Angka 1-100: ");
+        printf ("Guess number from 1-100: ");
 
         if (scanf("%d", &x) !=1){
-            printf("Tebak Angka Tolol!\n");
+            printf("It's guessing number you stupid!\n");
             while (getchar() != '\n');
             continue;
         }
 
         if (x == y ){
-            printf("Akhirnya Bener Tolol!\n");
+            printf("Finally you got it right, you idiot!\n");
         }
             else{
-                printf("Tolol! Coba lagi.\n");
+                printf("Idiot! Try again.\n");
             }
         }
     return 0;
